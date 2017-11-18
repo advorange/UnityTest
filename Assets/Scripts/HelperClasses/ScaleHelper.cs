6032bf7dd3978b61fdbaa4374169a1625fe9f8ed
Advorange <advorange@live.com>
2017-11-18 02:31:42 -0800

    Finally made the method IgnoreParentScaling work.
    Instead of only working when the character is facing a set way it now works when facing any way.

--- Assets/Scripts/HelperClasses/ScaleHelper.cs
+++ Assets/Scripts/HelperClasses/ScaleHelper.cs
@@ -4,20 +4,49 @@ namespace Assets.Scripts.HelperClasses
 {
 	public static class ScaleHelper
 	{
+		/// <summary>
+		/// It's possible to just unattach the child then reattach after scaling,
+		/// but I'd rather do this the non cheaty way.
+		/// </summary>
+		/// <param name="gameObject"></param>
 		public static void IgnoreParentScaling(this GameObject gameObject)
 		{
 			var obj = gameObject.transform;
-			//Make the parent's local scale has the same angle as the child's rotation
-			var parentScale = obj.rotation * obj.parent.transform.localScale;
-			//Add another 180 onto those values so they are facing the correct way
-			var reflected = Vector3.Reflect(parentScale, Vector3.up);
+			//Got this through trial and error so don't expect any useful comments
+			var rotation = Quaternion.Inverse(obj.parent.rotation) * obj.rotation;
+			var rotatedScale = rotation * obj.parent.transform.localScale;
 			//Divide the child local scalings by the parent's to get rid of them
 			//Abs so nothing fires from the back of the gun
-			obj.localScale = new Vector3
+			obj.localScale = obj.localScale.DivideBy(rotatedScale).AbsValue();
+		}
+		/// <summary>
+		/// Divides every component in the first vector by the same component in the second vector.
+		/// If the vectors do not have the same rotation this may send back unwanted values.
+		/// </summary>
+		/// <param name="numerator"></param>
+		/// <param name="denominator"></param>
+		/// <returns></returns>
+		public static Vector3 DivideBy(this Vector3 numerator, Vector3 denominator)
+		{
+			return new Vector3
 			{
-				x = Mathf.Abs(obj.localScale.x / reflected.x),
-				y = Mathf.Abs(obj.localScale.y / reflected.y),
-				z = Mathf.Abs(obj.localScale.z / reflected.z),
+				x = numerator.x / denominator.x,
+				y = numerator.y / denominator.y,
+				z = numerator.z / denominator.z,
+			};
+		}
+		/// <summary>
+		/// Returns every component in the vector as positive.
+		/// </summary>
+		/// <param name="v"></param>
+		/// <returns></returns>
+		public static Vector3 AbsValue(this Vector3 v)
+		{
+			return new Vector3
+			{
+				x = Mathf.Abs(v.x),
+				y = Mathf.Abs(v.y),
+				z = Mathf.Abs(v.z),
 			};
 		}
 	}
--- Assets/Scripts/PlayerScripts/CameraMovement.cs
+++ Assets/Scripts/PlayerScripts/CameraMovement.cs
@@ -22,9 +22,8 @@ namespace Assets.Scripts.PlayerScripts
 			}
 			if (!InputHelper.MouseX().Equals(0.0f))
 			{
-				var x = InputHelper.MouseX() * Time.deltaTime * this.MoveSpeed;
-				var curAngles = this.Player.rotation.eulerAngles;
-				this.Player.rotation = Quaternion.Euler(curAngles.x, curAngles.y + x, curAngles.z);
+				var x = InputHelper.MouseX() * Time.deltaTime * this.MoveSpeed * (Screen.currentResolution.width / 500.0f);
+				this.Player.rotation = CreateNewRotation(this.Player.rotation.eulerAngles, x);
 			}
 			/*if (!InputHelper.MouseY().Equals(0.0f))
 			{
@@ -33,8 +32,21 @@ namespace Assets.Scripts.PlayerScripts
 				this.Player.rotation = Quaternion.Euler(curAngles.x + y, curAngles.y, curAngles.z);
 			}*/
 
-			this.transform.position = this.Player.position + this.Player.rotation * this.Offset;
+			this.transform.position = this.Player.position + (this.Player.rotation * this.Offset);
 			this.transform.rotation = this.Player.rotation;
+		}
+		private Quaternion CreateNewRotation(Vector3 euler, float val, int index = 1)
+		{
+			var v = new Vector3();
+			for (int i = 0; i < 3; ++i)
+			{
+				v[i] = euler[i];
+				if (i == index)
+				{
+					v[i] += val;
+				}
+			}
+			return Quaternion.Euler(v);
 		}
 	}
 }
